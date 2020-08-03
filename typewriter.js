e32a5eff98a5ad6b2b9df7a4c676a7b89618ab1c
Emma Bindi <emma.bindi@outlook.com>
2020-08-03 11:46:33 +1000

    Typed hello mesage added

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  var typed = new Typed('#typed', {
+    strings: ['Hi, I\'m Emma Bindi', 'I\'m a Developer'],
+    typeSpeed: 40,
+    backSpeed: 40,
+    fadeOut: true,
+    loop: true
+  })
+});
